109f26778dcd47e3f14ee3fa36b293bf4f454de1
David <sanderdj90@gmail.com>
2023-06-06 23:24:51 -0600

    push changes

--- index.js
+++ index.js
@@ -33,30 +33,68 @@ const getAnchors = async (page) => {
   return anchors;
 };
 
-(async () => {
-  let json = {};
+const getProjects = async (page) => {
+  const projects = await page.evaluate(() => {
+    const links = Array.from(
+      document.querySelectorAll(".grid-flow-row a"),
+      (element) => ({
+        title: element.href.split("/")?.pop(-1),
+      })
+    );
+    return links;
+  });
 
+  return projects;
+};
+
+const BASE_URL = "https://credits.meowwolf.com";
+let json = {};
+
+(async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://credits.meowwolf.com", { waitUntil: "load" });
+  await page.goto(BASE_URL, { waitUntil: "load" });
 
   const exhibitions = await getExhibitions(page);
   json["name"] = "exhibitions";
-  json["children"] = []
+  json["children"] = [];
 
   exhibitions.forEach((exhibit) => {
     json["children"].push({ name: exhibit.title, children: [] });
   });
+
+  let anchors = [];
   for (let i = 0; i < exhibitions.length; i++) {
-    await page.goto(exhibitions[i].slug);
-    const anchors = await getAnchors(page);
-    anchors.forEach(anchor => {
-      json.children?.[i].children.push({name: anchor.title, children: []})
-    })
+    await page.goto(exhibitions[i].slug, { waitUntil: "load" });
+    anchors = await getAnchors(page);
+
+    for (let j = 0; j < anchors.length; j++) {
+      json.children?.[i].children.push({
+        name: anchors[j].title,
+        children: [],
+      });
+      const url = `${exhibitions[i].slug}/${anchors[j].href}`;
+      console.log(url);
+      await page.goto(url, { waitUntil: "load" });
+      const elements = await page.$$("li button");
+      // Get the last selected element
+      const lastElement = elements[elements.length - 1];
+
+      // Click on the last selected element
+      await lastElement.click();
+      const projects = await getProjects(page);
+      for (let k = 0; k < projects.length; k++) {
+        json.children?.[i].children?.[j].children.push({
+          name: projects[k].title,
+          size: 100,
+        });
+      }
+      console.log({ projects });
+    }
   }
-  console.log(JSON.stringify(json, null, 2));
-const jsonData = JSON.stringify(json, null, 2);
+
+  const jsonData = JSON.stringify(json, null, 2);
 
   fs.writeFile("data.json", jsonData, (err) => {
     if (err) {
